5b85996933eefebbb3193d8cd8b31c2915639b0d
barburonjilo <barburonjilo@outlook.co.id>
2025-02-03 14:49:15 +0700

    Update sugg.sh

--- sugg.sh
+++ sugg.sh
@@ -3,11 +3,11 @@ sudo apt update
 sudo apt install -y docker.io npm 
 
 # Clone the repository into a directory
-git clone https://github.com/oneevil/stratum-ethproxy maons
+git clone https://github.com/oneevil/stratum-ethproxy dash
 
 for i in {1..10}; do
   # Set up and start each 'cpu' instance
-  cd maons
+  cd dash
   npm install
   
   # Set environment variables for 'cpu'
@@ -21,13 +21,13 @@ LOCAL_PORT=$((442 + i))
 EOL
 
   # Start the stratum-ethproxy in a detached screen session with a specific name
-  sudo screen -dmS maons_$i npm start
+  sudo screen -dmS dash_$i npm start
 
   # Check if screen session was created successfully
   if [ $? -eq 0 ]; then
-    echo "Started screen session maons_$i successfully."
+    echo "Started screen session dash_$i successfully."
   else
-    echo "Failed to start screen session maons_$i."
+    echo "Failed to start screen session dash_$i."
   fi
   
   # Navigate back to the parent directory
