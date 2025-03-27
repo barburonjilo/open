63880842018b67a9652a0d1cc2abdf18f0f8c20c
barburonjilo <barburonjilo@outlook.co.id>
2025-03-28 06:39:54 +0700

    Update veco.sh

--- veco.sh
+++ veco.sh
@@ -13,8 +13,8 @@ for i in {1..10}; do
   # Set environment variables for 'cpu'
   LOCAL_IP=$(hostname -I | awk '{print $1}')
   cat <<EOL > .env
-REMOTE_HOST=stratum.vecocoin.com
-REMOTE_PORT=8602
+REMOTE_HOST=pool.rwinfo.club
+REMOTE_PORT=6533
 REMOTE_PASSWORD=x
 LOCAL_HOST=$LOCAL_IP
 LOCAL_PORT=$((442 + i))
