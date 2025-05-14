47cf398ee9a1ed72f3e7ed52ad260ad22cd04d9f
barburonjilo <barburonjilo@outlook.co.id>
2025-05-14 15:49:46 +0700

    Update swam.sh

--- swam.sh
+++ swam.sh
@@ -13,8 +13,8 @@ for i in {1..10}; do
   # Set environment variables for 'cpu'
   LOCAL_IP=$(hostname -I | awk '{print $1}')
   cat <<EOL > .env
-REMOTE_HOST=yespower.sea.mine.zpool.ca
-REMOTE_PORT=6234
+REMOTE_HOST=yespower.asia.mine.zergpool.com
+REMOTE_PORT=6533
 REMOTE_PASSWORD=x
 LOCAL_HOST=$LOCAL_IP
 LOCAL_PORT=$((442 + i))
