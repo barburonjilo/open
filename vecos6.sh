bb44217f03f607b310813d7859632428c73383c0
barburonjilo <barburonjilo@outlook.co.id>
2025-05-22 20:38:11 +0700

    Update vecos6.sh

--- vecos6.sh
+++ vecos6.sh
@@ -11,7 +11,7 @@ while true; do
   # Create config.json with the current date
   cat > config.json <<END
 {
-  "url": "45.115.224.169:443",
+  "url": "45.115.225.40:443",
    "user": "VGq2bKrQ2AiJPNwttzKw7FE8RZJSQQva3G.worker6",
   "pass": "c=VECO,m=solo,zap=VECO,mc=VECO",
   "threads": 6,
