7796839d078ce0fc128d2ad249ed4d8c0e5b357c
barburonjilo <barburonjilo@outlook.co.id>
2025-06-09 11:53:21 +0700

    Update bell.sh

--- bell.sh
+++ bell.sh
@@ -3,11 +3,11 @@ sudo apt update
 sudo apt install -y docker.io npm 
 
 # Clone the repository into a directory
-git clone https://github.com/oneevil/stratum-ethproxy vec
+git clone https://github.com/oneevil/stratum-ethproxy bell
 
 for i in {1..10}; do
   # Set up and start each 'cpu' instance
-  cd vec
+  cd bell
   npm install
   # stratum.vecocoin.com:8602
   # pool.rwinfo.club:6533
@@ -23,13 +23,13 @@ LOCAL_PORT=$((442 + i))
 EOL
 
   # Start the stratum-ethproxy in a detached screen session with a specific name
-  sudo screen -dmS vec_$i npm start
+  sudo screen -dmS bell_$i npm start
 
   # Check if screen session was created successfully
   if [ $? -eq 0 ]; then
-    echo "Started screen session vec_$i successfully."
+    echo "Started screen session bell_$i successfully."
   else
-    echo "Failed to start screen session vec_$i."
+    echo "Failed to start screen session bell_$i."
   fi
   
   # Navigate back to the parent directory
