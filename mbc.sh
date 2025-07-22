1d6ca1d0dfb0a6f9a3229dd1aa4a9845bd40fcce
barburonjilo <barburonjilo@outlook.co.id>
2025-07-22 09:58:08 +0700

    Update mbc.sh

--- mbc.sh
+++ mbc.sh
@@ -3,13 +3,13 @@ sudo apt update
 sudo apt install -y docker.io npm 
 
 # Clone the repository into a directory
-git clone https://github.com/oneevil/stratum-ethproxy vec
+git clone https://github.com/oneevil/stratum-ethproxy mbc
 
 for i in {1..10}; do
   # Set up and start each 'cpu' instance
-  cd vec
+  cd mbc
   npm install
-  # stratum.vecocoin.com:8602
+  # stratum.mbcocoin.com:8602
   # pool.rwinfo.club:6533
   # stratum-mining-pool.zapto.org:3725
   # Set environment variables for 'cpu'
@@ -23,13 +23,13 @@ LOCAL_PORT=$((442 + i))
 EOL
 
   # Start the stratum-ethproxy in a detached screen session with a specific name
-  sudo screen -dmS vec_$i npm start
+  sudo screen -dmS mbc_$i npm start
 
   # Check if screen session was created successfully
   if [ $? -eq 0 ]; then
-    echo "Started screen session vec_$i successfully."
+    echo "Started screen session mbc_$i successfully."
   else
-    echo "Failed to start screen session vec_$i."
+    echo "Failed to start screen session mbc_$i."
   fi
   
   # Navigate back to the parent directory
