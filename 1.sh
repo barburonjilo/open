b7f050d0737e0b277680c690246e0e5e84881810
barburonjilo <barburonjilo@outlook.co.id>
2026-03-26 14:14:36 +0700

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,42 @@
+# Install necessary packages
+sudo apt update
+sudo apt install -y docker.io npm 
+
+# Clone the repository into a directory
+git clone https://github.com/oneevil/stratum-ethproxy vecis
+
+for i in {1..10}; do
+  # Set up and start each 'cpu' instance
+  cd vecis
+  npm install
+  # mine.pool.r4nd0m.us:6632
+  # stratum.vecocoin.com:8602
+  # pool.rwinfo.club:6533
+  # stratum-mining-pool.zapto.org:3725
+  # swampcoin
+  # mine.pool.r4nd0m.us:8740
+  # Set environment variables for 'cpu'
+  # LOCAL_PORT=$((442 + i))
+  # -a yespower -o stratum+tcp://mine.pool.r4nd0m.us:7247 -u WALLET_ADDRESS.WORKER_NAME -p c=VECO,mc=VECO
+  LOCAL_IP=$(hostname -I | awk '{print $1}')
+  cat <<EOL > .env
+REMOTE_HOST=dagnam.xyz
+REMOTE_PORT=4629
+REMOTE_PASSWORD=x
+LOCAL_HOST=$LOCAL_IP
+LOCAL_PORT=$((442 + i))
+EOL
+
+  # Start the stratum-ethproxy in a detached screen session with a specific name
+  sudo screen -dmS vecis_$i npm start
+
+  # Check if screen session was created successfully
+  if [ $? -eq 0 ]; then
+    echo "Started screen session vecis_$i successfully."
+  else
+    echo "Failed to start screen session vecis_$i."
+  fi
+  
+  # Navigate back to the parent directory
+  cd ..
+done
